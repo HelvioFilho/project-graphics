e94f3ce9ad1e5e74916c99c3845fb5d5fcec2fa6
Hélvio Filho <helviosvf@gmail.com>
2021-05-06 23:41:23 -0300

    Group by search

--- backend/src/main/java/com/hsvf/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/hsvf/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.hsvf.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.hsvf.dsvendas.dto.SaleSuccessDTO;
+import com.hsvf.dsvendas.dto.SaleSumDTO;
 import com.hsvf.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("SELECT new com.hsvf.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.hsvf.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
